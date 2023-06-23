032af502ca30cfc8235fdd4beaeb5276b0a9594f
MelnychenkoJenny <melnichenkojenny@gmail.com>
2023-06-23 11:54:22 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,4 @@
-import { createSelector } from '@reduxjs/toolkit';
+// import { createSelector } from '@reduxjs/toolkit';
 
 export const selectContacts = state => state.contacts.array;
 export const selectFilter = state => state.filter.query;
@@ -8,12 +8,12 @@ export const selectError = state => state.contacts.error;
 
 
 
-export const selectVisibleContacts = createSelector(
-  [selectContacts, selectFilter],
-  (contacts, filter) => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  }
-);
+// export const selectVisibleContacts = createSelector(
+//   [selectContacts, selectFilter],
+//   (contacts, filter) => {
+//     const normalizedFilter = filter.toLowerCase();
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(normalizedFilter)
+//     );
+//   }
+// );
